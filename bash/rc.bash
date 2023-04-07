c6a418d0503fe2ba7a9ad070de359ae619e60ab6
kriskoribsky <kiko.koribsky@gmail.com>
2023-04-07 13:22:58 +0200

    update rc.bash

--- bash/rc.bash
+++ bash/rc.bash
@@ -67,4 +67,9 @@ then
 fi
 export PATH
 
+# Node version manager
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 unset rc
