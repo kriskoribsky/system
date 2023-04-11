b931f82b44a1d97ad4c75a3144cf25b999e52949
kriskoribsky <kiko.koribsky@gmail.com>
2023-04-11 22:00:06 +0200

    add 'term' alias

--- bash/modules/aliases.bash
+++ bash/modules/aliases.bash
@@ -2,4 +2,6 @@ alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
 
-alias open=xdg-open
+alias open='xdg-open'
+
+alias term='function _f() { gnome-terminal --quiet --working-directory="$(realpath ${1:-$HOME})"; }; _f'
