a9f3413b4b0534e4a49fbea6805ffde440f2ce40
kriskoribsky <kiko.koribsky@gmail.com>
2023-04-12 20:56:57 +0200

    add xclip to the cli toolchain

--- bash/modules/aliases.bash
+++ bash/modules/aliases.bash
@@ -5,3 +5,5 @@ alias l='ls -CF'
 alias open='xdg-open'
 
 alias term='function _f() { gnome-terminal --quiet --working-directory="$(realpath ${1:-$HOME})"; }; _f'
+
+alias xclip="xclip -selection clipboard"
