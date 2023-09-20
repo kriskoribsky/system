c58504de50455c28380f1e0f95d2333fd239b745
kriskoribsky <kristian.koribsky@gmail.com>
2023-09-20 21:11:02 +0200

    fix: get actual user's home instead of root's

--- config.sh
+++ config.sh
@@ -3,8 +3,11 @@
 if [[ ${BOOTSTRAP_CONFIG_LOADED:-} -ne 1 ]]; then
     readonly BOOTSTRAP_CONFIG_LOADED=1
 
+    BOOTSTRAP_SYSTEM_USER_HOME=$(getent passwd "$SUDO_USER" | cut -d: -f6)
+    export BOOTSTRAP_SYSTEM_USER_HOME
+
     export BOOTSTRAP_SYSTEM_REPO='https://github.com/ksoft-system/system.git'
-    export BOOTSTRAP_SYSTEM_LOCATION="$HOME/system"
+    export BOOTSTRAP_SYSTEM_LOCATION="$BOOTSTRAP_SYSTEM_USER_HOME/system"
 
     export BOOTSTRAP_INSTALLATION_SCRIPT='install.sh'
     export BOOTSTRAP_INSTALLATION_REPORT='bootstrap-report.txt'
