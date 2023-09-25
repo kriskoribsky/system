002b0893d9979a504d8d770f21248733cc58190f
kriskoribsky <90533421+kriskoribsky@users.noreply.github.com>
2023-09-25 17:08:03 +0200

    refactor: remove utils from config

--- config.sh
+++ config.sh
@@ -3,8 +3,6 @@
 # directory of this script
 dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-source "$dir/utils/path.sh"
-
 if [[ ${SYSTEM_CONFIG_LOADED:-} -ne 1 ]]; then
     readonly SYSTEM_CONFIG_LOADED=1
 
@@ -14,7 +12,6 @@ if [[ ${SYSTEM_CONFIG_LOADED:-} -ne 1 ]]; then
     SYSTEM_DIR_ROOT="$dir"
     SYSTEM_DIR_MODULES="$dir/modules"
     SYSTEM_DIR_PRIORTIY="$dir/priority"
-    SYSTEM_DIR_UTILS="$dir/utils"
     SYSTEM_DIR_BIN="$dir/bin"
 
     SYSTEM_PATH_CONFIG=$(readlink -f "$dir/config.sh")
