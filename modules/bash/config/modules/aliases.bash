a1d2844bbbbe3acf96354c2aea826a4475564968
kriskoribsky <90533421+kriskoribsky@users.noreply.github.com>
2023-09-27 12:57:29 +0200

    feat: change default la, ls, ll aliases for better custom experience

--- modules/bash/config/modules/aliases.bash
+++ modules/bash/config/modules/aliases.bash
@@ -1,9 +1,17 @@
 # Add an "alert" alias for long running commands.  Use like so: 'sleep 10; alert'
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
+alias ls='ls --color=auto --group-directories-first --human-readable'
+alias ll='ls -l --almost-all --size'
+
+alias tree='tree --dirsfirst'
+
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
 
 alias open='xdg-open'
 
@@ -12,8 +20,6 @@ alias text='gnome-text-editor'
 alias term='function _f() { gnome-terminal --quiet --working-directory="$(realpath ${1:-$HOME})"; }; _f'
 
 alias clip='xclip -selection clipboard'
-
-alias tree='tree --dirsfirst'
 
 alias mkdir='mkdir -pv'
 
--- modules/bash/config/modules/prompt.bash
+++ modules/bash/config/modules/prompt.bash
@@ -8,18 +8,6 @@ PROMPT_COMMAND='if [ $? = 0 ]; then
                     PS1="${PS} $(__git_ps1 "(%s) ")$SYSTEM_THEME_RESET${SYSTEM_THEME_FG_ERROR}$ ${SYSTEM_THEME_RESET}";
                 fi'
 
-# enable color support of ls and also add handy aliases
-if [ -x /usr/bin/dircolors ]; then
-    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-    alias ls='ls --color=auto'
-    alias dir='dir --color=auto'
-    alias vdir='vdir --color=auto'
-
-    alias grep='grep --color=auto'
-    alias fgrep='fgrep --color=auto'
-    alias egrep='egrep --color=auto'
-fi
-
 # show only the last directory in path
 export PROMPT_DIRTRIM=1
 
