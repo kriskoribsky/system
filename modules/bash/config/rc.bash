e55af2353fd11d3ce34e6eb3a614ac42cc7f951e
kriskoribsky <90533421+kriskoribsky@users.noreply.github.com>
2023-09-27 17:15:40 +0200

    fix: bash autocompletion

--- modules/bash/config/rc.bash
+++ modules/bash/config/rc.bash
@@ -25,19 +25,14 @@ shopt -s checkwinsize
 # match all files and zero or more directories and subdirectories.
 shopt -s globstar
 
-# enable programmable completion features (you don't need to enable
-# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
-# sources /etc/bash.bashrc).
+# enable bash completion in interactive shells
 if ! shopt -oq posix; then
   if [ -f /usr/share/bash-completion/bash_completion ]; then
-    source /usr/share/bash-completion/bash_completion
+    . /usr/share/bash-completion/bash_completion
   elif [ -f /etc/bash_completion ]; then
-    source /etc/bash_completion
+    . /etc/bash_completion
   fi
 fi
-
-# enable autocomplete after sudo command 
-complete -cf sudo
 
 # Make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
