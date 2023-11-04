16743e51897ec99fb7a5ec36f81ece0b9042e425
kriskoribsky <90533421+kriskoribsky@users.noreply.github.com>
2023-11-04 20:01:46 +0100

    feat: add automatic background install script for gnome module

--- modules/gnome/install.sh
+++ modules/gnome/install.sh
@@ -5,3 +5,15 @@ dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 # load configured gnome settings
 dconf load -f / <"$dir/config/dconf-full.ini"
+
+# add system backgrounds to gnome settings
+system_backgrounds_dir="$dir/img"
+gnome_backgrounds_dir="$HOME/.local/share/backgrounds"
+
+cp "$system_backgrounds_dir"/* "$gnome_backgrounds_dir"/
+
+# set default system background
+system_default_background="$gnome_backgrounds_dir/space-3.jpg"
+
+gsettings set org.gnome.desktop.background picture-uri "$system_default_background"
+gsettings set org.gnome.desktop.background picture-uri-dark "$system_default_background"
