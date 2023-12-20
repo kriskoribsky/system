dd0ca484a5b6b08eae1d01ed3b25f563db2d96b0
kriskoribsky <90533421+kriskoribsky@users.noreply.github.com>
2023-12-20 22:54:18 +0100

    feat: add description

--- bin/help.sh
+++ bin/help.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-# Displays information about this cli-tool and lists available commands
+# Displays information about this cli-tool and lists available commands.
--- bin/specs.sh
+++ bin/specs.sh
@@ -1,1 +1,3 @@
 #!/usr/bin/env bash
+
+# Display information about system hardware.
